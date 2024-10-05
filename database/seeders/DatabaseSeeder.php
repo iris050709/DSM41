d7d81df6b1305573cce30d1bcf73cc58847507de
iris050709 <ic8281932@gmail.com>
2024-10-05 09:33:59 -0600

    051024_2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        $this->call(BookSeederExample::class);
+        //$this->call(BookSeederExample::class);
 
-        $this->call(BookSeeder::class);
+        //$this->call(BookSeeder::class);
         /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);*/
-        $this->call(ProductosSeeder::class);
+        //$this->call(ProductosSeeder::class);
+        $this->call(class: UserSeeder::class);
     }
 }
