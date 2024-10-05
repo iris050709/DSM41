76edcc82ebe89268398168bc2622f63ec5cd0d21
iris050709 <ic8281932@gmail.com>
2024-10-05 09:06:13 -0600

    051024

--- resources/views/vistas/list_users.blade.php
+++ resources/views/vistas/list_users.blade.php
@@ -1,18 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('title', 'Listado de usuarios')
+@section('styles')
+    <link rel="stylesheet" href="hola.css">
+@endsection
+@section('content')
     <h1>USER LIST</h1>
     <ul>
     @foreach($usuarios as $usuario)
-        <li>{{ $usuario->name }} </li>
+    <!--<i class="bi bi-2-square-fill"></i>--><li>{{ $usuario->name }} </li>
     @endforeach
-    {{ $usuarios->links() }}
+    {{ $usuarios->links('pagination::bootstrap-4') }}
     </ul>
-</body>
-</html>
+@endsection
